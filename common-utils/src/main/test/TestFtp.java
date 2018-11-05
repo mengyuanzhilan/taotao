9eafede19e7d48a6f99831b285aaee661adb4d7b
moyan <541382268@qq.com>
2018-11-06 01:21:34 +0800

    上传类

---
+++ common-utils/src/main/test/TestFtp.java
@@ -0,0 +1,48 @@
+
+
+import cn.zmt.FtpUtil;
+import org.apache.commons.net.ftp.FTPClient;
+import org.junit.Test;
+
+import java.io.*;
+
+/**
+ * @author zmt
+ * @date 2018/11/5 - 1:56
+ */
+public class TestFtp {
+    String ftpHost = "47.107.135.170";
+    String ftpUserName = "ftpuser";
+    String ftpPassword = "mengyuan";
+    int ftpPort = 21;
+
+    /**
+     * 上传
+     */
+    @Test
+    public void up(){
+        String ftpPath = "/images/";//ftp路径
+        String localPath = "i:/ailisha.jpg";//上传路径和文件名
+        String fileName = "ailisha.jpg";//上传后的文件名
+        try{
+            FileInputStream in=new FileInputStream(new File(localPath));
+            boolean test = FtpUtil.uploadFile(ftpHost, ftpUserName, ftpPassword, ftpPort, ftpPath, fileName,in);
+            System.out.println(test);
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+            System.out.println(e);
+        }
+    }
+
+    /**
+     * 下载
+     */
+    @Test
+    public void down(){
+        String ftpPath = "/images/";//ftp路径
+        String localPath = "e:/";//下载路径
+        String fileName = "ailisha.jpg";//文件名字
+        //下载一个文件
+        FtpUtil.downloadFtpFile(ftpHost, ftpUserName, ftpPassword, ftpPort, ftpPath, localPath, fileName);
+    }
+}
---
+++ taotao-manager/taotao-manager-web/src/main/test/cn/zmt/controller/Hell.java
@@ -0,0 +1,11 @@
+package cn.zmt.controller;
+
+/**
+ * @author zmt
+ * @date 2018/11/2 - 2:43
+ */
+public class Hell {
+    public static void main(String[] args) {
+        System.out.println("hell");
+    }
+}
